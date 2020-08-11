730ff0fd9ef567ab84976e84f1395066b9baa8a6
Jimmy_dingo <you@example.com>
2020-08-11 18:11:11 +0300

    cleaned and formatted

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,21 +5,22 @@ const Header = function () {
     const closedIcon = '/assets/icons/icon-menu-close.png';
     const openedIcon = '/assets/icons/hamburger-icon-menu-closed.png';
     const [showMenu, setShowMenu] = useState(false);
+    const [easeInAnim, setEaseInAnim] = useState(false);
 
     let hamburger;
     let mobileMenu;
 
     const mobileNav = (
-        <nav className="main-nav-mobile">
+        <nav className={`main-nav-mobile ${showMenu ? "mobile-menu-anim" : ""}`}>
 
-            <ul className="menu-pages">
+            <ul className={`menu-pages ${easeInAnim ? "animated-toggle-icon" : ""}`}>
                 <li><a href="/">About Me</a></li>
                 <li><a href="/my-online-cv">My online CV</a></li>
                 <li><a href="/portfolio">Portfolio</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
 
-            <div className="nav-ico">
+            <div className={`nav-ico ${showMenu ? "mobile-menu-anim" : ""}`}>
                 <a href="https://www.linkedin.com/in/gianmarco-procida/" target="_blank"><img className="icon-smedia" src="/assets/icons/linked-in.png" alt="linkedIn-icon" /></a>
                 <a href="https://github.com/Jimmy-dingo" target="_blank"><img className="icon-smedia" src="/assets/icons/gitHub.png" alt="gitHub-icon" /></a>
             </div>
@@ -31,12 +32,12 @@ const Header = function () {
     if (showMenu) {
         hamburger = closedIcon;
         mobileMenu = mobileNav;
+        console.log()
     }
 
     if (!showMenu) {
         hamburger = openedIcon;
     }
-
 
     return (
         <>
@@ -60,7 +61,7 @@ const Header = function () {
                 </nav>
 
                 <div className="main-nav-toggle">
-                    <img src={hamburger} className='toggle-icon' onClick={() => setShowMenu(!showMenu)} />
+                    <img src={hamburger} className={`toggle-icon ${easeInAnim ? "animated-toggle-icon" : ""}`} onClick={(e) => { setShowMenu(!showMenu); setEaseInAnim(!easeInAnim) }} />
 
                     {mobileMenu}
                 </div>
