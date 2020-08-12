d93eb608955d8934a43f969a0c2ae07f2513d1d7
Jimmy_dingo <you@example.com>
2020-08-12 14:56:23 +0300

    success message set + hover on buttons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,51 +2,57 @@ import React from 'react';
 import Form from './Form';
 import './Contact.css';
 
-const Contact = () => {
-    return (
-        <section className="Contact container-fluid">
+const Contact = function () {
+  return (
+    <section className="Contact container-fluid">
 
-            <div className="row">
-                <div className="left-column col-md-8">
-                    <div>
+      <div className="row">
 
-                        <h1>Gianmarco Procida</h1>
-                        <h2>Front-end Developer<br />and Designer</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-                        nascetur ridiculus mus.
-                        </p>
+        <div className="left-column col-md-8">
 
-                    </div>
+          <div>
 
-                    <div class="buttons-cont container">
+            <h1>Gianmarco Procida</h1>
+            <h2>Front-end Developer<br />and Designer</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+            nascetur ridiculus mus.</p>
 
-                        <button onClick={()=> window.location.href="/"}>About me</button>
-                        <button onClick={()=> window.location.href="/my-online-cv"}>My Resumee</button>
+          </div>
 
-                    </div>
+          <div class="buttons-cont container">
 
-                    <div>
-                    <p className="location container"><span><img src="/assets/icons/location-mark.png" alt="location-mark" /></span> Cluj-Napoca, Romania</p>
-                    </div>
+            <button onClick={() => window.location.href = "/"}>About me</button>
+            <button onClick={() => window.location.href = "/my-online-cv"}>My Resumee</button>
 
-                    <div className="media-icons">
+          </div>
 
-                        <a href="https://www.linkedin.com/in/gianmarco-procida/" target="_blank"><img className="icon-smedia-cont" src="/assets/icons/linked-in-onHover.png" alt="linkedIn-icon" /></a>
-                        <a href="https://github.com/Jimmy-dingo" target="_blank"><img className="icon-smedia-cont" src="/assets/icons/gitHub-onHover.png" alt="gitHub-icon" /></a>
-                        
+          <div>
 
-                    </div>
-                </div>
-                <div className="right-column col-md-4">
+            <p className="location container"><span><img src="/assets/icons/location-mark.png" alt="location-mark" /></span> Cluj-Napoca, Romania</p>
 
-                    <Form />
+          </div>
 
-                </div>
-            </div>
+          <div className="media-icons">
 
-        </section>
-    )
-}
+            <a href="https://www.linkedin.com/in/gianmarco-procida/" target="_blank"><img className="icon-smedia-cont" src="/assets/icons/linked-in-onHover.png" alt="linkedIn-icon" /></a>
+            <a href="https://github.com/Jimmy-dingo" target="_blank"><img className="icon-smedia-cont" src="/assets/icons/gitHub-onHover.png" alt="gitHub-icon" /></a>
+
+
+          </div>
+
+        </div>
+
+        <div className="right-column col-md-4">
+
+          <Form />
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+};
 
 export default Contact;
